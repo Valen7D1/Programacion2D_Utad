cd35e55669af72609a31d84317cb4d41639dfd4c
Valen <gvalentisanguino@gmail.com>
2024-04-22 19:48:20 +0200

    draw cpps

---
+++ Practica 3/src/draw.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <litegfx.h>
+
+class vec2;
+
+double deg2rad(double degrees);
+ltex_t* loadImg(const char* filename);
+void drawFullScreen(float width, float height, ltex_t* tex);
+void drawFillingGaps(float width, float height, vec2 mouseLocation, ltex_t* tex);
+void drawInLocation(vec2 location, ltex_t* tex);
+void drawRotatedSized(vec2 location, ltex_t* tex, float angleOfFire, float scale);
